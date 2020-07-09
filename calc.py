3036a89acf58fd0eb55264a7c33c841b8bbd4035
E-CoolPuppy <awang@dominicanacademy.org>
2020-07-09 16:29:13 -0400

    updated size function

--- calc.py
+++ calc.py
@@ -3,26 +3,32 @@
 # weight = input("What is your weight in pounds?")
 # gender = input("What is your gender?")
 
+# https://www.kohls.com/media/digital/ecom/size-charts/costumes/Costumes135.htm 
 #height, weight : size
 girls_shirt_size = {
-    ("5'2", 53): "small",
-    ("5'5", 69): "small",
-    ("5'6", 70): "medium",
-    ("5'9", 84): "medium",
-    ("5'10", 85): "large",
-    ("6'0", 99): "large"
+    ("5'0", 100): "small",
+    ("5'3", 120): "small",
+    ("5'6", 135): "medium",
+    ("5'8", 150): "medium",
+    ("5'10", 160): "large",
+    ("5'11", 170): "large"
 }
 
 def height_to_inches(height):
-    return (int(height.split("'")[0])*6) + int(height.split("'")[1])
+    return (int(height.split("'")[0])*12) + int(height.split("'")[1])
+
+# def size(height, weight, gender):
+#     height = height_to_inches(height)
+#     for i in girls_shirt_size:
+#         for j in girls_shirt_size:
+#             if (height_to_inches(i[0]) <= height) and (i[1] <= weight) and (height_to_inches(j[0]) > height) and (j[1] > weight):
+#                 return girls_shirt_size[j]
 
 def size(height, weight, gender):
     height = height_to_inches(height)
     for i in girls_shirt_size:
-        for j in girls_shirt_size:
-            if (height_to_inches(i[0]) <= height) and (i[1] <= weight) and (height_to_inches(j[0]) > height) and (j[1] > weight):
-                return girls_shirt_size[j]
+        if (height_to_inches(i[0]) <= height) and (i[1] <= weight) and (height_to_inches(i[0]) + 3 > height) and (i[1] + 16 > weight):
+            return girls_shirt_size[i]
 
 
-
-print(size("5'3", 60, "female"))
+print(size("5'7", 80, "female"))
